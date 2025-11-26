28531fe4fcedd3044e33928d0018f12c3e11d2b8
Deepansh Umar <umardeepansh@gmail.com>
2025-11-26 11:34:39 +0530

    Milestone-VP-MAD2 Charts-Analytics

---
+++ frontend/src/components/SummaryChart.vue
@@ -0,0 +1,29 @@
+<template>
+  <canvas ref="canvas"></canvas>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue"
+import { Chart, BarController, BarElement, CategoryScale, LinearScale } from "chart.js"
+
+Chart.register(BarController, BarElement, CategoryScale, LinearScale)
+
+const props = defineProps(["labels", "values"])
+
+const canvas = ref(null)
+
+onMounted(() => {
+  new Chart(canvas.value, {
+    type: "bar",
+    data: {
+      labels: props.labels,
+      datasets: [
+        {
+          label: "Parking Cost (₹)",
+          data: props.values
+        }
+      ]
+    }
+  })
+})
+</script>
---
+++ frontend/src/router/index.js
@@ -0,0 +1,38 @@
+import { createRouter, createWebHistory } from "vue-router"
+
+import Login from "../views/auth/Login.vue"
+import Register from "../views/auth/Register.vue"
+
+import UserDashboard from "../views/user/UserDashboard.vue"
+import UserSummary from "../views/user/UserSummary.vue"
+
+import AdminDashboard from "../views/admin/AdminDashboard.vue"
+import AdminLots from "../views/admin/AdminLots.vue"
+import AdminSummary from "../views/admin/AdminSummary.vue"
+import AdminUsers from "../views/admin/AdminUsers.vue"
+import AdminCreateLot from "../views/admin/AdminCreateLot.vue"  
+import AdminLotEdit from "../views/admin/AdminLotEdit.vue" 
+import AdminLotSpots from "../views/admin/AdminLotSpots.vue"
+
+const routes = [
+  { path: "/", redirect: "/login" },
+  { path: "/login", component: Login },
+  { path: "/register", component: Register },
+
+  { path: "/user/dashboard", component: UserDashboard },
+  { path: "/user/summary", component: UserSummary },
+
+  { path: "/admin/dashboard", component: AdminDashboard },
+  { path: "/admin/lots", component: AdminLots },
+  { path: "/admin/lots/create", component: AdminCreateLot },  
+  { path: "/admin/users", component: AdminUsers },
+  { path: "/admin/summary", component: AdminSummary },
+  { path: "/admin/lots/edit/:id", component: AdminLotEdit },
+  { path : "/admin/lots/:id/spots", component : AdminLotSpots},
+
+]
+
+export default createRouter({
+  history: createWebHistory(),
+  routes
+})
