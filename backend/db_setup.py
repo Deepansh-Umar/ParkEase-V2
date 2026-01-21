67d96b860456985700389f3122d5872971aae821
Deepansh Umar <umardeepansh@gmail.com>
2026-01-21 21:34:37 +0530

    moved admin creation creds to env

--- backend/db_setup.py
+++ backend/db_setup.py
@@ -1,3 +1,4 @@
+import os
 from extensions import db
 from models import User
 # from app import app
@@ -8,12 +9,12 @@ def setup_db(app):
     with app.app_context():
         db.create_all()
 
-        if not User.query.filter_by(username="admin").first():
+        if not User.query.filter_by(username=os.environ.get("ADMIN_USERNAME")).first():
             admin = User(
                 id="admins",
-                username="admin",
-                email="admin@example.com",
-                password=generate_password_hash("admin123"),
+                username=os.environ.get("ADMIN_USERNAME"),
+                email=os.environ.get("ADMIN_EMAIL"),
+                password=generate_password_hash(os.environ.get("ADMIN_PASSWORD")),
                 role="admin"
             )
             db.session.add(admin)
